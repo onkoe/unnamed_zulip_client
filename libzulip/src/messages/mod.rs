94d721d924a34246300ed20d6832d3bb92c494de
Barrett <onkoe@protonmail.com>
2024-10-01 18:49:31 -0500

    refactor(lib/msg): group the msg modules

--- libzulip/src/messages/mod.rs
+++ libzulip/src/messages/mod.rs
@@ -1,9 +1,13 @@
 // each module represents one API call. this allows for lots of strong-typed
 // happiness without the massive overload of random types everywhere
+
+// message modules
 pub mod delete_message;
-pub mod download_file; // note: this isn't an api call. it's here for sanity
 pub mod edit_message;
-pub mod emoji_reaction; // contains both add and remove calls
 pub mod fetch_single_message;
-pub mod send;
-pub mod upload_file;
+pub mod send_message;
+
+// media modules
+pub mod download_file;
+pub mod emoji_reaction; // contains both add and remove calls
+pub mod upload_file; // note: this isn't an api call. it's here for sanity

17bfefee9f47110638fcc0cfba39a8ad7a830dda
Barrett <onkoe@protonmail.com>
2024-10-01 19:38:04 -0500

    feat(lib/error): add nice ResponseError type
    minimizes boilerplate

--- libzulip/src/error.rs
+++ libzulip/src/error.rs
@@ -1,6 +1,26 @@
 use pisserror::Error;
 use std::error::Error;
 
+/// An error that potentially appears in a response.
+///
+/// ... ok, you! maintainer! this is an error that we check for in any response.
+/// it should ALWAYS be `serde::flatten`, since responses don't contain an error
+/// cool as that would be.
+///
+/// you should also make it an `Option<ResponseError>` in the response type.
+#[derive(Clone, Debug, PartialEq, PartialOrd, Eq, Ord, serde::Deserialize)]
+pub(crate) struct ResponseError {
+    code: String,
+    msg: String,
+}
+
+impl std::fmt::Display for ResponseError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "err({}): {}", self.code, self.msg)
+    }
+}
+
+/// The main error type for this crate.
 #[derive(Debug, Error)]
 pub enum ZulipError {
     #[error("Error with API request. err: {_0}")]
@@ -15,6 +35,7 @@ pub enum ZulipError {
     MessageError(#[from] MessageError),
 }
 
+/// Errors from file upload/download.
 #[derive(Clone, Debug, Error)]
 pub enum FileError {
     #[error("The given file was not found on disk. (path: `{_0}`)")]
@@ -31,12 +52,15 @@ pub enum FileError {
     AttachSerializeFailed(String),
 }
 
+/// Errors when performing messaging tasks.
 #[derive(Clone, Debug, Error)]
 pub enum MessageError {
     #[error("Failed to send the given message. content: `{content}`, err: {error_code}")]
     SendFailed { content: String, error_code: String },
+
     #[error("Failed to delete the message with ID `{id}`. err: {error_code}")]
     DeletionFailed { id: u64, error_code: String },
+
     #[error(
         "Couldn't add an emoji reaction to message `{msg_id}` with emoji name `{emoji_name}`. err: {error_code}"
     )]
@@ -45,6 +69,7 @@ pub enum MessageError {
         emoji_name: String,
         error_code: String,
     },
+
     #[error(
         "Couldn't remove an emoji reaction to message `{msg_id}` with emoji name `{emoji_name}`. err: {error_code}"
     )]
@@ -53,6 +78,10 @@ pub enum MessageError {
         emoji_name: String,
         error_code: String,
     },
+
     #[error("Failed to fetch the message with ID `{msg_id}`. err: {error_code}")]
     SingleMessageFetchFailed { msg_id: u64, error_code: String },
+
+    #[error("The server failed to render the following message: `{content}`. {error}")]
+    RenderMessageFailed { content: String, error: String },
 }
